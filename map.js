eb17388dea317de5a11e672ca66cbac8a2ae49d1
Imran <imranbinazad777@gmail.com>
2025-03-15 18:48:03 +0600

    files moved to apropriate folder

--- map.js
+++
@@ -1,9 +0,0 @@
-const mapSchema = new mongoose.Schema({
-    mapID: { type: String, required: true, unique: true },
-    locationName: { type: String, required: true },
-    coordinates: String,
-    activityFilters: [String],
-    directions: String,
-});
-
-module.exports = mongoose.model('Map', mapSchema);
--- photo.js
+++
@@ -1,9 +0,0 @@
-const photoSchema = new mongoose.Schema({
-    photoID: { type: String, required: true, unique: true },
-    userID: { type: String, required: true, ref: 'User' },
-    uploadDate: Date,
-    caption: String,
-    storagePath: String,
-});
-
-module.exports = mongoose.model('Photo', photoSchema);
