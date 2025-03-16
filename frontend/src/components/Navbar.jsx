be0a2b7a91b2a1c5aee9f49dff7ee62718e053f8
Imran <imranbinazad777@gmail.com>
2025-03-17 01:36:47 +0600

    Navbar completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu, X } from "lucide-react"; // For responsive menu icons
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+
+  const isSignedIn = useSelector((state)=> state.auth.isSignedIn)
+
+  const profilePhotoLink = useSelector(
+    (state) => state.auth.user?.profilePhoto || "/defaultUser.jpg"
+  );
+  
   return (
     <nav className="z-50 fixed top-0 left-0 right-0 h-16 backdrop-blur-md flex items-center justify-between px-5 md:px-10">
       {/* Brand Name */}
@@ -20,15 +28,20 @@ const Navbar = () => {
       {/* Search Bar */}
       <input 
         type="text"
-        className="hidden md:block w-64 md:w-80 h-8 border-2 border-gray-600 rounded-md px-2"
+        className="hidden md:block w-64 md:w-80 h-8 border-2 border-gray-600 text-gray-900 rounded-md px-2"
         placeholder="Search..."
       />
 
       {/* Login & Signup (Desktop) */}
       <div className="hidden md:flex space-x-5">
-        <Link className="text-lg font-semibold" to="/login">Log in</Link>
-        <Link className="text-lg font-semibold" to="/register">Sign up</Link>
+      {isSignedIn==false ? (
+        <>
+          <Link className="text-lg font-semibold" to="/login">Log in</Link>
+          <Link className="text-lg font-semibold" to="/register">Sign up</Link>
+        </>
+      ):(<img src={profilePhotoLink} className="h-10 w-10 rounded-full"/>)}
       </div>
+      
 
       {/* Mobile Menu Button */}
       <button onClick={() => setIsOpen(!isOpen)} className="md:hidden">
@@ -43,11 +56,15 @@ const Navbar = () => {
     <Link className="text-lg font-semibold text-gray-800" to="/hotels">Hotels</Link>
     <input 
       type="text"
-      className="w-4/5 h-8 border-2 border-gray-600 rounded-md px-2"
+      className="w-4/5 h-8 border-2 border-gray-600 text-gray-900 rounded-md px-2"
       placeholder="Search..."
     />
-    <Link className="text-lg font-semibold text-gray-800" to="/login">Log in</Link>
-    <Link className="text-lg font-semibold text-gray-800" to="/register">Sign up</Link>
+    {isSignedIn==false ? (
+        <>
+          <Link className="text-lg font-semibold text-gray-900" to="/login">Log in</Link>
+          <Link className="text-lg font-semibold text-gray-900" to="/register">Sign up</Link>
+        </>
+      ):<Link className="text-lg font-semibold text-gray-900" to="/profile">Profile</Link>}
   </div>
 )}
 
