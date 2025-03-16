319733679ad1a06b0464536de30af9c7bd3ba272
Imran <imranbinazad777@gmail.com>
2025-03-17 05:30:21 +0600

    home page and default user image added

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Welcome from '../components/Welcome'
+import Mytrips from './Mytrips'
 
 const Home = () => {
   return (
     <div className='w-screen h-auto flex flex-col items-center justify-start'>
       <Welcome/>
+      <Mytrips/>
     </div>
   )
 }
