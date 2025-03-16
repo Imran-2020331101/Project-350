2b7b05e594bbabd1defa71d86294b48aaba88616
Imran <imranbinazad777@gmail.com>
2025-03-16 16:00:04 +0600

    database schema changed to non relational

--- Database_Schemas/itinerary.js
+++ Database_Schemas/itinerary.js
@@ -1,13 +1,17 @@
+const mongoose = require("mongoose");
+
 const itinerarySchema = new mongoose.Schema({
-    itineraryID: { type: String, required: true, unique: true },
-    userID: { type: String, required: true, ref: 'User' },
-    destination: { type: String, required: true },
-    startDate: Date,
-    endDate: Date,
-    transportDetails: String,
-    hotelDetails: String,
-    mealRecommendations: String,
-    costBreakdown: String,
+  itineraryID: { type: String, required: true, unique: true },
+  userID: { type: String, required: true },
+  destination: { type: String, required: true },
+  startDate: Date,
+  timePeriod: Number,
+  mealRecommendations: [{
+    mealName: String,
+    mealImageLink:String,
+    mealDescription:String
+  }],
+  cost: Number
 });
 
-module.exports = mongoose.model('Itinerary', itinerarySchema);
+module.exports = mongoose.model("itinerary", itinerarySchema);
--- Database_Schemas/user.js
+++ Database_Schemas/user.js
@@ -5,8 +5,8 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    phoneNumber: [String],
-    profilePicture: String,
+    phoneNumber: String,
+    profilePictureLink: String,
 });
 
 module.exports = mongoose.model('User', userSchema);
