ed1a4d3c15b79ada1f4865dc4aab1339ee017df7
Imran <imranbinazad777@gmail.com>
2025-03-18 05:21:06 +0600

    chaged before reverting

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Welcome from '../components/Welcome'
-import Mytrips from './Mytrips'
 
 const Home = () => {
   return (
     <div className='w-screen h-auto flex flex-col items-center justify-start'>
       <Welcome/>
-      <Mytrips/>
     </div>
   )
 }
